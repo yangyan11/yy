65ff29043df88061b7120661019ce96eb176fa99
yangyan11 <542214450@qq.com>
2016-12-18 22:42:49 +0800

    hunlian

---
+++ js/index.js
@@ -0,0 +1,15 @@
+window.onload=function(){
+    function resize(originSize,type){
+        var type=type||"x";
+        var widths=document.documentElement.clientWidth;
+        var heights=document.documentElement.clientHeight;
+        if(type=="x"){
+            var scale=widths/originSize*100;
+        }else if(type=="y"){
+            var scale=heights/originSize*100;
+        }
+        document.getElementsByTagName("html")[0].style.fontSize=scale+"px";
+
+    }
+    resize(1334,"y");
+};
